76a317e9d075cf4b15af7501f89ee1462d8056a2
Jlaporte25 <jacoblaporte25@gmail.com>
2025-06-12 22:40:18 -0500

    continued

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,3 +1,9 @@
+import os
+from os.path import isdir
+
 def get_files_info(working_directory, directory=None):
+    path = os.path.abspath(directory)
     if directory not in working_directory:
         raise Exception(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+    if os.path.isdir(path):
+        raise Exception(f'Error: "{directory}" is not a directory')
