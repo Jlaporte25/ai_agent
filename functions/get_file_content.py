9659f6d863eb2c66e2c8d1ecbf43fc06f1ce3829
Jlaporte25 <jacoblaporte25@gmail.com>
2025-07-10 17:39:52 -0500

    continuing get file content

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,2 +1,12 @@
+import os
+
+
 def get_file_content(working_directory, file_path):
-    pass
+    abs_working_dir = os.path.abspath(working_directory)
+    if file_path:
+        target_dir = os.path.abspath(os.path.join(working_directory, file_path))
+        if not target_dir.startswith(abs_working_dir):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(file_path):
+            return f'Error: File is not found or is not a regular file: "{file_path}"'
+
