25a0a3d5ada2d9b391e42b726d9e988d90b4f79e
Jlaporte25 <jacoblaporte25@gmail.com>
2025-07-28 20:43:03 -0500

    continuing get file content

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,5 @@
 import os
-
+from config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
     abs_working_dir = os.path.abspath(working_directory)
@@ -9,4 +9,7 @@ def get_file_content(working_directory, file_path):
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
         if not os.path.isfile(file_path):
             return f'Error: File is not found or is not a regular file: "{file_path}"'
+
+    with open(file_path, "r") as f:
+        file_content_string = f.read(MAX_CHARS)
 
