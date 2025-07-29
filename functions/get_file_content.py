48819f4d5253c9678aed20a4c1740cf962bdd88f
Jlaporte25 <jacoblaporte25@gmail.com>
2025-07-28 23:08:28 -0500

    finished get_file_content function

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,15 +1,23 @@
 import os
-from config import MAX_CHARS
+from config import *
 
 def get_file_content(working_directory, file_path):
     abs_working_dir = os.path.abspath(working_directory)
-    if file_path:
-        target_dir = os.path.abspath(os.path.join(working_directory, file_path))
-        if not target_dir.startswith(abs_working_dir):
-            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-        if not os.path.isfile(file_path):
-            return f'Error: File is not found or is not a regular file: "{file_path}"'
+    target_file = os.path.abspath(os.path.join(working_directory, file_path))
+    if not target_file.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(target_file):
+        return f'Error: File is not found or is not a regular file: "{file_path}"'
 
-    with open(file_path, "r") as f:
-        file_content_string = f.read(MAX_CHARS)
+    try:
+        with open(target_file, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if f.read(1):
+                result = file_content_string + f"[...File '{file_path}' truncated at 10000 characters]"
+                return result    
+            else:
+                return file_content_string
+    except Exception as e:
+        return f"Error: {e}"
+
 
