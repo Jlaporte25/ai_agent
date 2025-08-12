5caa559dcd004e392e9f431a5ccb7d2362670683
Jlaporte25 <jacoblaporte25@gmail.com>
2025-08-11 21:13:58 -0500

    added to run_python_file

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -6,3 +6,7 @@ def run_python_file(working_directoy, file_path, args=[]):
 
     if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(abs_file_path):
+        return f'Error: File "{file_path}" not found.'
+    if not abs_file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a python file.'
