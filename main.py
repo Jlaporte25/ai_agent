d0c81eaecf27359772cc5f7e158ac7d29ea79158
Jlaporte25 <jacoblaporte25@gmail.com>
2025-10-12 15:29:41 -0500

    added a system prompt and started function declaration functionality

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 def main():
     if len(sys.argv) > 1:
         user_prompt = sys.argv[1]
+        system_prompt = """
+        You are a helpful AI coding agent.
+
+        When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+        - List files and directories
+
+        All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+        """
 
         messages = [
             types.Content(role="user", parts=[types.Part(text=user_prompt)]),
@@ -19,8 +29,16 @@ def main():
 
         response = client.models.generate_content(
             model='gemini-2.0-flash-001',
-            contents=messages
+            contents=messages,
+            config=types.GenerateContentConfig(system_instruction=system_prompt),
         )
+
+        available_functions = types.Tool(
+                funtion_declarations=[
+                    schema_get_files_info,
+                    ]
+                )
+
         if len(sys.argv) > 2:
             if sys.argv[2] == "--verbose":
                 print(f"User prompt: {user_prompt}")
